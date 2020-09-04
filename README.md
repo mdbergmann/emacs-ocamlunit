8b0cf3e52521e3a8e92591a08229f31b02d2e3cd
Manfred Bergmann <mb@software-by-mabe.com>
2020-09-04 12:56:52 +0200

    initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# emacs-ocamlunit
+
+This is a Emacs minor mode implementation for to run OUnit2 tests or other tests in a OCaml `dune` project.
+
+Tests are actually run with `dune test`. So also other test definitions than OUnit/OUnit2 are run also.
+
+There is no package on Elpa or Melpa.
+To install it clone this to some local folder and initialize like this in Emacs:
+
+```
+(use-package ocamlunit
+  :load-path "~/.emacs.d/plugins/ocamlunit")
+```
+
+When done you have a minor mode called `ocamlunit-mode`.
+
+This mode can be enabled for basically every buffer but only `tuareg-mode` buffers are supported.
+On other code or project it just saves the buffer.
+
+To run the tests a buffer use the key sequence: `C-t c`.
+This will first save the buffer and then run the tests using `dune`.
+
+After the first execution of `ocamlunit-execute` you can view the "OCamlUnit output" buffer for test output.
---
+++ ocamlunit.el
@@ -0,0 +1,84 @@
+;;; ocamlunit.el --- OUnit2 test runner -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Manfred Bergmann.
+
+;; Author: Manfred Bergmann <manfred.bergmann@me.com>
+;; URL: http://github.com/mdbergmann/ocamlunit
+;; Version: 0.1
+;; Keywords: processes ocaml ounit2 ounit
+;; Package-Requires: ((emacs "24.3"))
+
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Provides commands to run tests that are run with `dune test'.
+
+;;; Code:
+
+(make-variable-buffer-local
+ (defvar ocamlunit-mode))
+
+(defun execute-ocaml-test ()
+  "Call OCaml test via 'dune'."
+  (let* ((test-cmd-args (list "opam" "exec" "dune" "test"))
+         (call-args
+          (append (list (car test-cmd-args) nil "OCamlUnit output" t)
+                  (cdr test-cmd-args))))
+    (message "calling: %s" call-args)
+    (let* ((default-directory (locate-dominating-file default-directory "dune-project"))
+           (call-result (apply 'call-process call-args)))
+      (message "cwd: %s" default-directory)
+      (message "test call result: %s" call-result)
+      call-result)))
+
+(defun handle-successful-test-result ()
+  "Do some stuff when the tests ran OK.")
+
+(defun handle-unsuccessful-test-result ()
+  "Do some stuff when the tests ran NOK.")
+
+(defun after-save-action ()
+  "Execute the test."
+  (message "ocamlunit: after save action from in: %s" major-mode)
+
+  (let ((test-result (cond
+                      ((string-equal "tuareg-mode" major-mode)
+                       (execute-ocaml-test))
+                      (t (progn (message "Unknown mode!")
+                                nil)))))
+
+    (unless (eq test-result nil)
+      (if (= test-result 0)
+          (handle-successful-test-result)
+        (handle-unsuccessful-test-result)))))
+
+(defun ocamlunit-execute ()
+  "Save buffers and execute dune to run the tests."
+  (interactive)
+  (save-buffer)
+  (save-some-buffers)
+  (after-save-action))
+
+(define-minor-mode ocamlunit-mode
+  "OCaml - OUnit/OUnit2 test runner. Actually `dune test' is run. So this might catch more tests than only OUnit"
+  :lighter " OCamlUnit"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-t c") 'ocamlunit-execute)
+            map))
+
+(provide 'ocamlunit)
+;;; ocamlunit.el ends here
